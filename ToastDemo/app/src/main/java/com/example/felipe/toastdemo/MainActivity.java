cd19995de7630c5b64b656cd0e2dc7e6e78ba5fb
Felipe <felipemoraes920@gmail.com>
2018-06-12 21:22:24 -0300

    Toast demo

---
+++ ToastDemo/app/src/main/java/com/example/felipe/toastdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.felipe.toastdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void login(View view){
+
+        EditText name = findViewById(R.id.nameText);
+
+        String showText = "Hello " + name.getText().toString();
+
+        Toast.makeText(this, showText, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
